9fc49a5437dd9329eb82baa8ebcb0ed56d0c3bfc
Maxrutto <ruttomaxmillan@gmail.com>
2024-10-07 17:26:05 +0300

    Don't make a promise...if you know you can't keep it

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      let obj = {
+        status: 200,
+        body: 'Success'
+      };
+      resolve(obj);
+    } else {
+      let err = new Error("The fake API is not working currently");
+      reject(err);
+    }
+  });
+}
