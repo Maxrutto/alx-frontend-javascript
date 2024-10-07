cdfc40a868fb3381c194ac81d1706f1c80bd7392
Maxrutto <ruttomaxmillan@gmail.com>
2024-10-07 18:28:33 +0300

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,7 +5,7 @@ export default function handleResponseFromAPI(promise) {
       return { status: 200, body: 'success' }
     })
     .catch(() => {
-      return Error()
+      return new Error()
     })
     .finally(() => console.log("Got a response from the API"))
 }
