f64336ebc4b185d0ffa41ebbd3fd169ccfa5648a
Maxrutto <ruttomaxmillan@gmail.com>
2025-02-03 14:36:42 +0300

    Update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,13 @@
-import { uploadPhoto, createUser } from './utils.js'
+import { uploadPhoto, createUser } from "./utils.js";
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((value) => {
-      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    .then((values) => {
+      const [photo, user] = values;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
 
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => {
+      console.log("Signup system offline");
+    });
 }
